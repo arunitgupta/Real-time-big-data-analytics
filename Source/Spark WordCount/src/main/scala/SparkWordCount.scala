c4af49c474486037cdffd81f7dff2768aff20567
Arunit Gupta <arunitgupta@gmail.com>
2016-08-31 23:00:35 -0500

    Source Code
    Commited by Gupta

---
+++ Source/Spark WordCount/src/main/scala/SparkWordCount.scala
@@ -0,0 +1,36 @@
+
+
+import org.apache.spark.{SparkContext, SparkConf}
+
+
+/**
+ * Created by Arunit on 008/29/2016.
+ */
+object SparkWordCount {
+
+  def main(args: Array[String]) {
+
+    System.setProperty("hadoop.home.dir", "C:\\winutils");
+
+    val sparkConf = new SparkConf().setAppName("SparkWordCount").setMaster("local[*]")
+
+    val sc = new SparkContext(sparkConf)
+
+    val input = sc.textFile("input")
+
+    val wc = input.flatMap(line => {line.split(" . ")}).map(word => (word, 1)).cache()
+
+
+    val output = wc.reduceByKey(_ + _)
+    output.saveAsTextFile("output")
+
+    val o = output.collect()
+
+    var s: String = "Words:Count \n"
+    o.foreach { case (word, count) => {
+      s += word + " : " + count + "\n"
+    }
+    }
+
+
+  }}
---
+++ Source/Spark WordCount/src/sorting.sc
@@ -0,0 +1,8 @@
+/**
+  * Created by arunit on 8/31/2016.
+  */
+
+val b = List("Hi How are you.",
+  "I am good, fine thank you.",
+  "This is Arunit Gupta.",
+"Arunit works and study in UMKC.").sorted
